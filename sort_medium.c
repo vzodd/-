ecf0fbf9397543f2b3374d1b86cc97fa6415a0a4
jorge <jorgevilchezexposito@gmail.com>
2026-06-08 11:00:38 +0200

    .

--- sort_medium.c
+++ sort_medium.c
@@ -1,111 +1,104 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_medium.c                                      :+:      :+:    :+:   */
+/*   sort_medium1.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jorvilch <jorvilch@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/22 11:56:37 by jorvilch          #+#    #+#             */
-/*   Updated: 2026/06/08 09:04:59 by jorvilch         ###   ########.fr       */
+/*   Created: 2026/06/08 10:01:19 by jorvilch          #+#    #+#             */
+/*   Updated: 2026/06/08 10:58:36 by jorvilch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	find_max_pos(t_stack *b)
+int	get_chunk_size(int total_size)
 {
-	int	max;
+	if (total_size <= 16)
+		return (8);
+	if (total_size <= 100)
+		return (20);
+	return (50);
+}
+
+void	push_chunks_to_b(t_stack *a, t_stack *b)
+{
+	int	i;
+	int	chunk_size;
+
+	i = 0;
+	chunk_size = get_chunk_size(a->size);
+	while (a->top >= 0)
+	{
+		if (a->arr[a->top] <= i)
+		{
+			pb(a, b);
+			i++;
+		}
+		else if (a->arr[a->top] <= i + chunk_size)
+		{
+			pb(a, b);
+			rb(b, a);
+			i++;
+		}
+		else
+			ra(a);
+	}
+}
+
+int	find_max_pos(t_stack *stack)
+{
+	int	max_val;
 	int	max_pos;
 	int	i;
 
-	max = b->arr[0];
-	max_pos = 0;
-	i = 1;
-	while (i <= b->top)
+	i = stack->top;
+	max_val = stack->arr[i];
+	max_pos = i;
+	while (i >= 0)
 	{
-		if (b->arr[i] > max)
+		if (stack->arr[i] > max_val)
 		{
-			max = b->arr[i];
+			max_val = stack->arr[i];
 			max_pos = i;
 		}
-		i++;
+		i--;
 	}
 	return (max_pos);
 }
 
-static void	move_max_to_top(t_stack *b, t_stack *a)
+void	push_max_to_a(t_stack *a, t_stack *b)
 {
-	int	pos;
-	int	size;
+	int	max_pos;
+	int	steps;
 
-	pos = find_max_pos(b);
-	size = b->top + 1;
-	if (pos <= size / 2)
+	max_pos = find_max_pos(b);
+	if (b->top - max_pos <= b->top / 2)
 	{
-		pos = size - pos;
-		while (pos-- > 0)
-			rrb(b, a);
+		steps = b->top - max_pos;
+		while (steps > 0)
+		{
+			rb(b, a);
+			steps--;
+		}
 	}
 	else
-		while (pos-- > 0)
-			rb(b, a);
-}
-
-static void	push_chunks(t_stack *a, t_stack *b,
-	int chunk_min, int chunk_max)
-{
-	int	size;
-	int	i;
-
-	while (1)
 	{
-		size = a->top + 1;
-		i = size - 1;
-		while (i >= 0 && (a->arr[i] < chunk_min || a->arr[i] > chunk_max))
-			i--;
-		if (i < 0)
-			break ;
-		if (i >= size / 2)
-			while (peek(a) < chunk_min || peek(a) > chunk_max)
-				ra(a);
-		else
-			while (peek(a) < chunk_min || peek(a) > chunk_max)
-				rra(a);
-		pb(a, b);
-		if (peek(b) < chunk_min + (chunk_max - chunk_min) / 2)
-			rb(b, a);
+		steps = max_pos + 1;
+		while (steps > 0)
+		{
+			rrb(b, a);
+			steps--;
+		}
 	}
-}
-
-static void	push_to_a(t_stack *a, t_stack *b)
-{
-	while (b->top >= 0)
-	{
-		move_max_to_top(b, a);
-		pa(a, b);
-	}
+	pa(a, b);
 }
 
 void	sort_medium(t_stack *a, t_stack *b)
 {
-	int	size;
-	int	chunk_size;
-	int	chunk_min;
-	int	chunk_max;
-
-	size = a->top + 1;
-	normalize_stack(a);
-	chunk_size = (int)sqrt((double)size);
-	if (chunk_size < 1)
-		chunk_size = 1;
-	chunk_min = 0;
-	while (chunk_min < size)
+	push_chunks_to_b(a, b);
+	while (b->top >= 0)
 	{
-		chunk_max = chunk_min + chunk_size - 1;
-		if (chunk_max >= size)
-			chunk_max = size - 1;
-		push_chunks(a, b, chunk_min, chunk_max);
-		chunk_min += chunk_size;
+		push_max_to_a(a, b);
 	}
-	push_to_a(a, b);
 }
